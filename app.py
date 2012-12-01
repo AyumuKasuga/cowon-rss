9a36cfb6e92fb85f28f7ecfc6a9b488e68f0acb4
Ayumu Kasuga <bps@dzen.eu>
2012-12-02 02:55:31 +0400

    redis as storage for page

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+r = redis.from_url(os.environ['REDISTOGO_URL'])
+
 
 @app.route('/')
 def index():
-    root_path = os.path.dirname(os.path.abspath(__file__))
-    static_path = os.path.abspath(os.path.join(root_path, 'static'))
-    return open(static_path + '/index.html', 'r').read()
+    return r.get('index.html', 'None')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
